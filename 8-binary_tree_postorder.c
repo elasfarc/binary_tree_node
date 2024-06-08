5a274476150277f6f20752512765ac7b78b9a86c
Mohammed Al-Asfar <elasfarc@gmail.com>
2024-06-08 03:36:55 -0500

    Add #binary_tree_inorder && #binary_tree_postorder

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -13,7 +13,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func)
 		return;
 
-	binary_tree_inorder(tree->left);
-	binary_tree_inorder(tree->right);
+	binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->right, func);
 	func(tree->n);
 }
